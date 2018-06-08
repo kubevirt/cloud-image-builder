c46bde14a076b673773106f871bd89ae9f5ff2dc
Richard Su <rwsu@redhat.com>
2018-06-08 10:03:00 -0700

    Add virtctl to AMI, run update at end of AMI build
    Remove spot price from test playbook.

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 cd image-files
 git clone https://github.com/kubevirt/kubevirt-ansible/
+
+export KUBEVIRT_VERSION=$(cat kubevirt-ansible/vars/all.yml | grep version | grep -v _ver | cut -f 2 -d ' ')
+curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/v$KUBEVIRT_VERSION/virtctl-v$KUBEVIRT_VERSION-linux-amd64
+chmod +x virtctl
+
 cp cluster-localhost.yml kubevirt-ansible/playbooks/cluster/kubernetes
 cd ..
+
 $PACKER build $PACKER_BUILD_TEMPLATE
