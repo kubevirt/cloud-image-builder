03f120d102ed7791cb9fb7f19896e3aad21c9c5c
karmab <karimboumedhel@gmail.com>
2018-11-23 19:12:33 +0100

    deploy all components with custom script

--- build.sh
+++ build.sh
@@ -1,32 +1,11 @@
 #!/usr/bin/env bash
 set -x
-
-if [ ! -d kubevirt-ansible ]; then
-  git clone https://github.com/kubevirt/kubevirt-ansible
-
-  sed -i "s@kubectl taint nodes {{ ansible_fqdn }} node-role.kubernetes.io/master:NoSchedule- || :@kubectl taint nodes --all node-role.kubernetes.io/master-@"  kubevirt-ansible/roles/kubernetes-master/templates/deploy_kubernetes.j2
-
-  # set specific kubevirt version
-  sed -i 's@version: 0.9.5@version: 0.10.0@' kubevirt-ansible/vars/all.yml
-
-  #Fix for missing {{ }}
-  sed -i "s/weavenet.stdout/\"{{ weavenet.stdout }}\"/" kubevirt-ansible/roles/kubernetes-master/tasks/main.yml
-
-  # Fix for bad determination of openshift environment
-  cp ./patches/kubevirt.yml kubevirt-ansible/playbooks
-  cp ./patches/provision.yml kubevirt-ansible/roles/kubevirt/tasks
-fi
-
-export KUBEVIRT_VERSION=$(cat kubevirt-ansible/vars/all.yml | grep version | grep -v _ver | cut -f 2 -d ' ')
-cd image-files
-[ -f virtctl ] || curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/v$KUBEVIRT_VERSION/virtctl-v$KUBEVIRT_VERSION-linux-amd64
-chmod +x virtctl
-
-cp ../tests/pretest-checks.yml .
-cd ..
-
-echo $KUBEVIRT_VERSION > kubevirt-version
-pwd
+# sed -i 's@version: 0.9.5@version: 0.10.0@' kubevirt-ansible/vars/all.yml
+# sed -i "s/weavenet.stdout/\"{{ weavenet.stdout }}\"/" kubevirt-ansible/roles/kubernetes-master/tasks/main.yml
+# export KUBEVIRT_VERSION=$(cat kubevirt-ansible/vars/all.yml | grep version | grep -v _ver | cut -f 2 -d ' ')
+# [ -f virtctl ] || curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/v$KUBEVIRT_VERSION/virtctl-v$KUBEVIRT_VERSION-linux-amd64
+# cp ../tests/pretest-checks.yml .
+# echo $KUBEVIRT_VERSION > kubevirt-version
 
 $PACKER build -debug -machine-readable --force $PACKER_BUILD_TEMPLATE | tee build.log
 echo "AWS_TEST_AMI=`egrep -m1 -oe 'ami-.{8}' build.log`" >> job.props
--- image-files/first-boot-centos.sh
+++ image-files/first-boot-centos.sh
@@ -1,51 +1,95 @@
 #! /bin/sh
-export KUBEVIRT_ANSIBLE_DIR=/home/centos/kubevirt-ansible
-cd $KUBEVIRT_ANSIBLE_DIR
-echo "[masters]" >> inventory-aws
-hostname >> inventory-aws
+
+# set variables
+K8S="1.11.0"
+FLANNEL="v0.10.0"
+KUBEVIRT="v0.10.0"
+CDI="v1.3.0"
+URL="http://metadata/computeMetadata/v1/instance/attributes/"
+HEADER="X-Google-Metadata-Request: True"
+wget -O - $URL/k8s_version --header="$HEADER" > /tmp/x && K8S=`cat /tmp/x`
+wget -O - $URL/flannel_version --header="$HEADER" > /tmp/x && FLANNEL=`cat /tmp/x`
+# wget -O - $URL/kubevirt_version --header="$HEADER" > /tmp/x && KUBEVIRT=`cat /tmp/x`
+wget -O - $URL/kubevirt_version --header="$HEADER" > /tmp/x 
+if [ "$?" == "0" ] ; then 
+KUBEVIRT=`cat /tmp/x`
+else
+KUBEVIRT=`curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest| jq -r .tag_name`
+fi
+# wget -O - $URL/cdi_version --header="$HEADER" > /tmp/x && CDI=`cat /tmp/x`
+wget -O - $URL/cdi_version --header="$HEADER" > /tmp/x
+if [ "$?" == "0" ] ; then 
+CDI=`cat /tmp/x`
+else
+CDI=`curl -s https://api.github.com/repos/kubevirt/containerized-data-importer/releases/latest| jq -r .tag_name`
+fi
 
 # make sure we use a weave network that doesnt conflict
-for num in `seq 30 50` ; do
- ip r | grep -q 172.$num
- if [ "$?" != "0" ] ; then
-  sed -i "s/172.30/172.$num/" playbooks/roles/kubernetes-master/vars/main.yml
-  break
- fi
-done
+# for num in `seq 30 50` ; do
+# ip r | grep -q 172.$num
+# if [ "$?" != "0" ] ; then
+#  WEAVENETWORK="172.30/172.$num/24"
+#  break
+# fi
+# done
 
-sudo ansible-playbook playbooks/cluster/kubernetes/cluster-localhost.yml --connection=local -i inventory-aws
+# deploy kubernetes
+echo net.bridge.bridge-nf-call-iptables=1 >> /etc/sysctl.d/99-sysctl.conf
+sysctl -p
+setenforce 0
+sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
+yum install -y docker kubelet-${K8S} kubectl-${K8S} kubeadm-${K8S}
+sed -i "s/--selinux-enabled //" /etc/sysconfig/docker
+systemctl enable docker && systemctl start docker
+systemctl enable kubelet && systemctl start kubelet
+CIDR="10.244.0.0/16"
+kubeadm init --pod-network-cidr=${CIDR}
+cp /etc/kubernetes/admin.conf /root/
+chown root:root /root/admin.conf
+export KUBECONFIG=/root/admin.conf
+echo "export KUBECONFIG=/root/admin.conf" >>/root/.bashrc
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# enable kubectl for centos user
-sudo cp /etc/kubernetes/admin.conf /home/centos
-sudo chown centos:centos /home/centos/admin.conf
-export KUBECONFIG=/home/centos/admin.conf
-echo "export KUBECONFIG=~/admin.conf" >> /home/centos/.bash_profile
+# deploy flannel
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/${FLANNEL}/Documentation/kube-flannel.yml
+mkdir -p /root/.kube
+cp -i /etc/kubernetes/admin.conf /root/.kube/config
+chown root:root /root/.kube/config
 
-# wait for kubernetes cluster to be up
-sudo ansible-playbook /home/centos/cluster-wait.yml --connection=local
+# deploy dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/alternative/kubernetes-dashboard.yaml
+kubectl create clusterrolebinding kubernetes-dashboard-head --clusterrole=cluster-admin --user=system:serviceaccount:kube-system:kubernetes-dashboard
 
-# enable software emulation
-grep -q -E 'vmx|svm' /proc/cpuinfo || kubectl apply -f /home/centos/emulation-configmap.yaml
+# deploy kubevirt 
+yum -y install xorg-x11-xauth virt-viewer wget
+kubectl config set-context `kubectl config current-context` --namespace=kube-system
+grep -q vmx /proc/cpuinfo || kubectl create configmap -n kube-system kubevirt-config --from-literal debug.useEmulation=true
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT}/kubevirt.yaml
+wget https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT}/virtctl-${KUBEVIRT}-linux-amd64
+mv virtctl-${KUBEVIRT}-linux-amd64 /usr/bin/virtctl
+chmod u+x /usr/bin/virtctl
 
-# deploy kubevirt
-sudo ansible-playbook playbooks/kubevirt.yml -e@vars/all.yml -e cluster=kubernetes --connection=local -i inventory-aws
+# deploy cdi
+kubectl create ns golden
+kubectl create clusterrolebinding cdi --clusterrole=edit --user=system:serviceaccount:golden:default
+kubectl create clusterrolebinding cdi-apiserver --clusterrole=cluster-admin --user=system:serviceaccount:golden:cdi-apiserver
+wget https://github.com/kubevirt/containerized-data-importer/releases/download/${CDI}/cdi-controller.yaml
+sed -i "s/namespace:.*/namespace: golden/" cdi-controller.yaml
+kubectl apply -f cdi-controller.yaml -n golden
+kubectl expose svc cdi-uploadproxy -n golden
 
-# validate kubevirt pods and services are up
-cd /home/centos
-sudo ansible-playbook pretest-checks.yml --connection=local -i inventory-aws
-rm pretest-checks.yml
+# deploy kubevirt ui
+kubectl create namespace kweb-ui
+kubectl create clusterrolebinding kweb-ui --clusterrole=edit --user=system:serviceaccount:kweb-ui:default
+kubectl apply -f https://gist.githubusercontent.com/karmab/1ed94b351ad9728979ba1f4a6dd91e0f/raw/08d15b174a2782c365d8b52ff6323c771e0a50e8/ui.yml -n kweb-ui
 
-# remove CDI because users will create it as a lab exercise
-kubectl delete -f /tmp/cdi-provision.yml
+# set default context
+kubectl config set-context `kubectl config current-context` --namespace=default
 
 # generate motd
-cd /home/centos
-sudo ansible-playbook motd.yml -v
-rm motd*
-
-# cleanup
-rm cluster-wait.yml
-rm emulation-configmap.yaml
+# cd /home/centos
+# sudo ansible-playbook motd.yml -v
+# rm motd*
 
 # disable the service so it only runs the first time the VM boots
 sudo chkconfig kubevirt-installer off
