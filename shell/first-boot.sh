5306c6d06e3dc4569889b6ff1f92cfeeaf79f82d
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-10-22 10:03:52 +0200

    Use curl and adjust spacing (#129)
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- shell/first-boot.sh
+++ shell/first-boot.sh
@@ -1,50 +1,47 @@
 #! /bin/sh
 
 ######################
-## This script is a one-time script used upon boot to establish a Kubevirt-enabled single 
-## node "cluster" for development/testing purposes. Operations are done here (rather than 
+## This script is a one-time script used upon boot to establish a Kubevirt-enabled single
+## node "cluster" for development/testing purposes. Operations are done here (rather than
 ## in kb-image-builder) if they depend on details that may vary from VM instance-to-instance.
 ######################
 
-  ## Global Variables
+## Global Variables
 export INVENTORY_FILE="/tmp/inventory"
 export KUBEVIRT_ANSIBLE_PATH="/home/centos/kubevirt-ansible"
 export KUBEVIRT_VERSION="$(cat /home/centos/kubevirt-version)"
 export KUBECONFIG="/etc/kubernetes/admin.conf"
 export HOME="/root"
 
-  ## $KUBEVIRT_ANSIBLE_PATH is the default assumption
+## $KUBEVIRT_ANSIBLE_PATH is the default assumption
 cd $KUBEVIRT_ANSIBLE_PATH
 
-  ## Define "masters" group as consisting only of this node
+## Define "masters" group as consisting only of this node
 cat <<EOI > $INVENTORY_FILE
 [masters]
 $(hostname)
 EOI
 
-  ## TODO: fix this in a PR and remove this line
+## TODO: fix this in a PR and remove this line
 sed -i '/ipalloc_range:/ s/weavenet.stdout/"{{weavenet.stdout}}"/g' /home/centos/kubevirt-ansible/playbooks/cluster/kubernetes/roles/kubernetes-master/tasks/main.yml
 
-  ## Bootstrap a Kubernetes cluster using kubevirt-ansible
+## Bootstrap a Kubernetes cluster using kubevirt-ansible
 ansible-playbook -vvv playbooks/cluster/kubernetes/cluster-localhost.yml --connection=local -i $INVENTORY_FILE >/tmp/ansible-output.log 2>&1
 
  ## Wait for kubernetes cluster to be up
 i=0
 while [[ 1 ]]; do
-
-    ## Retrieve node status for current node
+  ## Retrieve node status for current node
   currentStatus=$(kubectl get nodes $(hostname) -o json | jq -r '.status.conditions[] | select(.reason == "KubeletReady") | .type')
 
-    ## Break if last status read was "Ready"
+  ## Break if last status read was "Ready"
   if [[ "$currentStatus" == "Ready" ]]; then
-
     break
-
   fi
 
   i=$(( $i + 1 ))
 
-    ## If this is the 25th iteration then exit the script with an error.
+  ## If this is the 25th iteration then exit the script with an error.
   if [[ $i -eq 26 ]]; then
     echo "Timed Out Waiting for Kubernetes To Become Available." >&2
     exit 1
@@ -54,20 +51,20 @@ while [[ 1 ]]; do
 
 done
 
- ## Deploy kubevirt
+## Deploy kubevirt
 kubectl create namespace kubevirt
 
-  ## If virt CPU flags aren't present, enable emulation 
-  ## More info: https://github.com/kubevirt/kubevirt/blob/master/docs/software-emulation.md
+## If virt CPU flags aren't present, enable emulation
+## More info: https://github.com/kubevirt/kubevirt/blob/master/docs/software-emulation.md
 grep -q -E 'vmx|svm' /proc/cpuinfo || kubectl create configmap -n kubevirt kubevirt-config --from-literal debug.useEmulation=true
 
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v${KUBEVIRT_VERSION}/kubevirt-operator.yaml 
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v${KUBEVIRT_VERSION}/kubevirt-operator.yaml
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v${KUBEVIRT_VERSION}/kubevirt-cr.yaml
 
 #mv /home/centos/after-install-checks.yml .
 #ansible-playbook after-install-checks.yml --connection=local -i $INVENTORY_FILE
 
- ## Generate The MOTD
+## Generate The MOTD
 KUBE_VERSION=$(kubectl get nodes $(hostname) -o json | jq -r .status.nodeInfo.kubeletVersion)
 
 cat <<EOI > /etc/motd
@@ -88,6 +85,6 @@ KubeVirt:   $KUBEVIRT_VERSION
 
 EOI
 
- ## Disable and delete everything so this only runs the first time the VM boots
+## Disable and delete everything so this only runs the first time the VM boots
 systemctl disable kubevirt-installer
 rm -rf /home/centos/kubevirt-ansible /usr/local/bin/first-boot-centos.sh /home/centos/kubevirt-version
