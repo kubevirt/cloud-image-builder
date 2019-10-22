209db9aeb74978737bcf950de7ae1da666a89cd4
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-10-22 10:01:01 +0200

    Use curl and adjust spacing
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- shell/publish.sh
+++ shell/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #################
-## Publish images to user accessible portions of their respective cloud providers. 
+## Publish images to user accessible portions of their respective cloud providers.
 ##
 ## Minikube is intentionally omitted since that's not intended for end user consumption and is just used for running tests.
 ## No real functional reason this file is separate from build.sh outside of letting Jenkins show this as being its own stage
@@ -11,15 +11,9 @@
 set -x
 
 if [[ -e ansible/${targetEnvironment}-publish.yml ]]; then
-
   if [[ "$targetEnvironment" == "gcp" ]]; then
-
     ansible-playbook -i /tmp/inventory --private-key ${SSH_KEY_LOCATION} ansible/${targetEnvironment}-publish.yml
-
   else
-
     ansible-playbook ansible/${targetEnvironment}-publish.yml > ansible-publish-${targetEnvironment}.log 2>&1
-
   fi
-
 fi
