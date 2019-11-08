f4bf738a4bb5859f7ccb938d640b63ec32c821f5
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-11-08 11:07:11 +0100

    Update log path
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- shell/build.sh
+++ shell/build.sh
@@ -10,6 +10,6 @@ rm -f bin/kubectl
 curl -L https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl -o bin/kubectl
 
 ## Execute required playbooks
-ansible-playbook ansible/${targetEnvironment}-provision.yml > ansible-provision-${targetEnvironment}.log 2>&1
-ansible-playbook --private-key ${SSH_KEY_LOCATION} -i /tmp/inventory ansible/${targetEnvironment}-setup.yml > ansible-setup-${targetEnvironment}.log 2>&1
-ansible-playbook --private-key ${SSH_KEY_LOCATION} -i /tmp/inventory ansible/${targetEnvironment}-mkimage.yml > ansible-mkimage-${targetEnvironment}.log 2>&1
+ansible-playbook ansible/${targetEnvironment}-provision.yml > ../ansible-provision-${targetEnvironment}.log 2>&1
+ansible-playbook --private-key ${SSH_KEY_LOCATION} -i /tmp/inventory ansible/${targetEnvironment}-setup.yml > ../ansible-setup-${targetEnvironment}.log 2>&1
+ansible-playbook --private-key ${SSH_KEY_LOCATION} -i /tmp/inventory ansible/${targetEnvironment}-mkimage.yml > ../ansible-mkimage-${targetEnvironment}.log 2>&1
